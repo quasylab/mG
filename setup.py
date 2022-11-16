7fcfc572c3f3c2a39bcb9bf5f05f1df24020c1e0
Matteo Belenchia <matteo.belenchia@unicam.it>
2022-11-16 17:12:43 +0100

    Automated testing setup

--- setup.py
+++ setup.py
@@ -1,13 +1,4 @@
 from setuptools import setup
 
-setup(
-    name='libmg',
-    version='1.0',
-    packages=['libmg'],
-    url='',
-    license='MIT',
-    author='Matteo Belenchia',
-    author_email='matteo.belenchia@unicam.it',
-    description='libmg package',
-    install_requires=['lark', 'numpy', 'scipy', 'spektral', 'tensorflow', 'tensorflow-gpu', 'tensorflow_addons']
-)
+if __name__ == '__main__':
+    setup()
