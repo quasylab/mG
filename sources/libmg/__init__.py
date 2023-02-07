9572a06905e7c24957e34a1a5b08c91567f79765
Matteo Belenchia <matteo.belenchia@unicam.it>
2023-02-07 18:14:02 +0100

    Added support for constant GNNs

--- sources/libmg/__init__.py
+++ sources/libmg/__init__.py
@@ -1,12 +1,12 @@
 # put the public interface here
-from .functions import PsiLocal, PsiGlobal, Psi, Phi, Sigma, FunctionDict
+from .functions import PsiLocal, PsiGlobal, Psi, Phi, Sigma, FunctionDict, Constant
 from .compiler import GNNCompiler, CompilationConfig, NodeConfig, EdgeConfig
 from .loaders import SingleGraphLoader, MultipleGraphLoader
 from .evaluator import PerformanceTest, PredictPerformance, CallPerformance, save_output_to_csv
 from .dataset import Dataset
 
 __all__ = ['SingleGraphLoader', 'MultipleGraphLoader', 'PerformanceTest', 'PredictPerformance', 'CallPerformance',
-           'save_output_to_csv', 'PsiLocal', 'PsiGlobal', 'Psi', 'Phi', 'Sigma', 'FunctionDict', 'GNNCompiler',
+           'save_output_to_csv', 'PsiLocal', 'PsiGlobal', 'Psi', 'Phi', 'Sigma', 'Constant', 'FunctionDict', 'GNNCompiler',
            'CompilationConfig', 'NodeConfig', 'EdgeConfig', 'Dataset']
 
-__version__ = '0.2.8'
+__version__ = '0.2.9'
