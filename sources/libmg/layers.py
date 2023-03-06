bb47bbd0de263daf9cf84e7f927c7edc868b98be
Matteo Belenchia <matteo.belenchia@unicam.it>
2023-03-06 13:39:40 +0100

    Removed relative tolerance from fixpoints

--- sources/libmg/layers.py
+++ sources/libmg/layers.py
@@ -119,7 +119,7 @@ class FixPoint(MessagePassing):
         super().__init__(**kwargs)
         self.gnn_x = gnn_x
         if precision is not None:
-            self.comparator = lambda curr, prev: tf.experimental.numpy.allclose(curr, prev, rtol=precision, atol=precision)
+            self.comparator = lambda curr, prev: tf.experimental.numpy.allclose(curr, prev, atol=precision)
         else:
             self.comparator = lambda curr, prev: curr == prev
 
