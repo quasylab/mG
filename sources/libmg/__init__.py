bf3cdfd69213185e767ee3aadfea2359ba93f1f3
Matteo Belenchia <matteo.belenchia@unicam.it>
2023-03-06 16:02:40 +0100

    Removed evaluator

--- sources/libmg/__init__.py
+++ sources/libmg/__init__.py
@@ -2,11 +2,9 @@
 from .functions import PsiLocal, PsiGlobal, Psi, Phi, Sigma, FunctionDict, Constant
 from .compiler import GNNCompiler, CompilationConfig, NodeConfig, EdgeConfig
 from .loaders import SingleGraphLoader, MultipleGraphLoader
-from .evaluator import PerformanceTest, PredictPerformance, CallPerformance, save_output_to_csv
 from .dataset import Dataset
 
-__all__ = ['SingleGraphLoader', 'MultipleGraphLoader', 'PerformanceTest', 'PredictPerformance', 'CallPerformance',
-           'save_output_to_csv', 'PsiLocal', 'PsiGlobal', 'Psi', 'Phi', 'Sigma', 'Constant', 'FunctionDict', 'GNNCompiler',
-           'CompilationConfig', 'NodeConfig', 'EdgeConfig', 'Dataset']
+__all__ = ['SingleGraphLoader', 'MultipleGraphLoader', 'PsiLocal', 'PsiGlobal', 'Psi', 'Phi', 'Sigma', 'Constant',
+           'FunctionDict', 'GNNCompiler', 'CompilationConfig', 'NodeConfig', 'EdgeConfig', 'Dataset']
 
-__version__ = '0.2.20'
+__version__ = '0.2.21'
