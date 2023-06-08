f73c6b383e6727b488f09907b42075ef02d674f6
Matteo Belenchia <matteo.belenchia@unicam.it>
2023-06-08 10:37:10 +0200

    Exported projection functions

--- sources/libmg/__init__.py
+++ sources/libmg/__init__.py
@@ -1,11 +1,11 @@
 # put the public interface here
-from .functions import PsiLocal, PsiGlobal, Psi, Phi, Sigma, Constant
+from .functions import PsiLocal, PsiGlobal, Psi, Phi, Sigma, Constant, Pi
 from .compiler import GNNCompiler, CompilationConfig, NodeConfig, EdgeConfig
 from .loaders import SingleGraphLoader, MultipleGraphLoader
 from .dataset import Dataset
 from .visualizer import print_layer
 
-__all__ = ['SingleGraphLoader', 'MultipleGraphLoader', 'PsiLocal', 'PsiGlobal', 'Psi', 'Phi', 'Sigma', 'Constant',
+__all__ = ['SingleGraphLoader', 'MultipleGraphLoader', 'PsiLocal', 'PsiGlobal', 'Psi', 'Phi', 'Sigma', 'Constant', 'Pi',
            'GNNCompiler', 'CompilationConfig', 'NodeConfig', 'EdgeConfig', 'Dataset', 'print_layer']
 
-__version__ = '0.3.31'
+__version__ = '0.3.32'
