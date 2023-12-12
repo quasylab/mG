a170a7c6df64dc5e4ea1eecc7a7cbba9ae3534f3
matteo <matteo.belenchia@unicam.it>
2023-12-12 15:39:10 +0100

    Removed run_tests from main package __init__

--- libmg/__init__.py
+++ libmg/__init__.py
@@ -7,10 +7,9 @@ from .data import Graph, Dataset, SingleGraphLoader, MultipleGraphLoader
 from .explainer import MGExplainer
 from .normalizer import mg_normalizer
 from .visualizer import print_graph, print_layer
-from .tests import run_tests
 
 __all__ = ['SingleGraphLoader', 'MultipleGraphLoader', 'PsiLocal', 'PsiGlobal', 'PsiNonLocal', 'Phi', 'Sigma', 'Constant', 'Pi',
            'MGCompiler', 'CompilerConfig', 'NodeConfig', 'EdgeConfig', 'Dataset', 'print_layer', 'print_graph', 'Graph',
-           'make_uoperator', 'make_boperator', 'make_koperator', 'MGExplainer', 'mg_normalizer', 'mg_reconstructor', 'mg_parser', 'run_tests']
+           'make_uoperator', 'make_boperator', 'make_koperator', 'MGExplainer', 'mg_normalizer', 'mg_reconstructor', 'mg_parser']
 
 __version__ = importlib.metadata.version('libmg')
