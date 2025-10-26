6add721e30bf95a8bdf3177210a99cc3decb4345
Matteo <matteo.belenchia@unicam.it>
2025-10-26 16:38:20 +0100

    Added verifier

--- libmg/__init__.py
+++ libmg/__init__.py
@@ -7,9 +7,11 @@ from .data import Graph, Dataset, SingleGraphLoader, MultipleGraphLoader
 from .explainer import MGExplainer
 from .normalizer import mg_normalizer
 from .visualizer import print_graph, print_layer, print_labels
+from .verifier import interpreter, run_abstract_model, check_soundness, AbstractionSettings, NoAbstraction, EdgeAbstraction, BisimAbstraction
 
 __all__ = ['SingleGraphLoader', 'MultipleGraphLoader', 'PsiLocal', 'PsiGlobal', 'PsiNonLocal', 'Phi', 'Sigma', 'Constant', 'Pi',
            'MGCompiler', 'CompilerConfig', 'NodeConfig', 'EdgeConfig', 'Dataset', 'print_layer', 'print_graph', 'print_labels', 'Graph',
-           'make_uoperator', 'make_boperator', 'make_koperator', 'MGExplainer', 'mg_normalizer', 'mg_reconstructor', 'mg_parser']
+           'make_uoperator', 'make_boperator', 'make_koperator', 'MGExplainer', 'mg_normalizer', 'mg_reconstructor', 'mg_parser',
+           'interpreter', 'run_abstract_model', 'check_soundness', 'AbstractionSettings', 'NoAbstraction', 'EdgeAbstraction', 'BisimAbstraction']
 
 __version__ = importlib.metadata.version('libmg')
